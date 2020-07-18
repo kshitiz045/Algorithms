3edc5e4270c2057118f9161775b4521312c489e2
Kshitiz Gupta <44299394+kshitiz045@users.noreply.github.com>
2020-07-18 20:30:35 +0530

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+#Developed By Kshitiz Gupta
+#Python program for implementation of Insertion Sort
+
+def insertion_sort(num):
+    for i in range(1 , len(num)):
+        key = num[i]
+        j = i-1
+        while (j>=0 and num[j]>key ):
+            num[j+1]=num[j]
+            j=j-1
+        num[j+1]=key
+
+num = input("Enter the elements ").split()
+insertion_sort(num)
+print('Sorted list: ', end='')
+print(num)
