25ce772cdaeaeee722e2c29169c6562fc7548904
Kshitiz Gupta <44299394+kshitiz045@users.noreply.github.com>
2020-07-21 17:05:36 +0530

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,14 @@
+
+#Python program fot hte implementation of selection sort
+
+arr = ['p' , 'y' , 't' , 'h' , 'o' , 'n']
+for i in range(len(arr)):
+   min_ar= i
+   for j in range(i+1, len(arr)):
+      if arr[min_ar] > arr[j]:
+         min_ar = j
+
+arr[i], arr[min_ar] = arr[min_ar], arr[i]
+
+for i in range(len(arr)):
+   print(arr[i])
